d7d2aa471be58cab1de80cd78ab0eeac469a1f47
Ivan Radzuik <94281718+Ivanessa00@users.noreply.github.com>
2025-01-29 15:35:08 +0300

    Time: 0 ms (100.00%) | Memory: 19.9 MB (20.79%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func lengthOfLastWord(_ s: String) -> Int {
+
+        var lastWord = ""
+
+        for char in s.reversed(){
+            if char != " "{
+                lastWord += String(char)
+            }else if lastWord.count > 0{
+                return lastWord.count
+            }
+        }
+    return lastWord.count
+    }
+}
