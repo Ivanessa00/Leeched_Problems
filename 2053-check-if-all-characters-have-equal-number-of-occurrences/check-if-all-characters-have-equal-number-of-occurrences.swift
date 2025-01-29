5c47ba540b2af29386e9ecb5464bd6ca8a4d7ea9
Ivan Radzuik <94281718+Ivanessa00@users.noreply.github.com>
2025-01-29 13:42:30 +0300

    Time: 4 ms (28.57%) | Memory: 20.2 MB (14.29%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.swift
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.swift
@@ -1,13 +1,11 @@
 class Solution {
     func areOccurrencesEqual(_ s: String) -> Bool {
-        var dictS = [Character: Int]()
-
+        var dictS = [Character:Int]()
         for char in s{
-            dictS[char,default:0] += 1
+            dictS[char, default: 0] += 1
         }
-
         let ar = Array(dictS.values)
 
-        return Set(ar).count == 1
+    return Set(ar).count == 1
     }
 }
